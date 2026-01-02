48eb8c15493e21ee43befac9043610b24c017431
Caleb Husovich <calebmhusovich@gmail.com>
2026-01-02 16:35:34 -0500

    adding camera packages based on git issue srufing

--- configuration.nix
+++ configuration.nix
@@ -17,16 +17,10 @@
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII23H7VPE8Fwz0y2dbWPGedl2uleLkscGlsC+Bi+oUxX calebmhusovich@gmail.com"
   ];
 
-  # Enable the Raspberry Pi camera module
-  hardware.raspberryPi.camera = {
-    enable = true;
-    firmware = true; # optional: ensures firmware blobs are present
-  };
- 
- # Camera packages
+ #Camera packages
   environment.systemPackages = [
-    pkgs.libcamera
-    pkgs.rpicam-apps
+    pkgs.rpi.libcamera
+    pkgs.rpi.rpicam-apps
     #nixos-raspberrypi.legacyPackages.${pkgs.system}.libcamera
     #nixos-raspberrypi.legacyPackages.${pkgs.system}.rpicam-apps
     pkgs.git
