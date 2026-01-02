016a79ed9b3269c913e92fe711093e0eeded18e8
Caleb Husovich <calebmhusovich@gmail.com>
2026-01-02 15:55:59 -0500

    trying to enable camera packages

--- configuration.nix
+++ configuration.nix
@@ -19,17 +19,12 @@
  
  # Camera packages
   environment.systemPackages = [
-    #pkgs.libcamera
-    #pkgs.rpicam-apps
+    pkgs.raspberrypi.libcamera
+    pkgs.raspberrypi.rpicam-apps
     #nixos-raspberrypi.packages.${pkgs.system}.libcamera
     #nixos-raspberrypi.legacyPackages.${pkgs.system}.rpicam-apps
   ];
-  
-  hardware.raspberry-pi."4".camera.enable = true;
-
-  # Trying to fix ipa path error for rpicam
-  #environment.variables.LIBCAMERA_IPA_PATH = "${pkgs.libcamera}/lib/libcamera/ipa";
- 
+   
   # Network Configuration
   networking = {
     firewall.enable = false;
