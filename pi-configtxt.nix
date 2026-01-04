af98aa3de464e78995802d3c4d3fffb3d7c65c11
Caleb Husovich <147654545+chusovich@users.noreply.github.com>
2026-01-04 00:10:57 -0500

    Update pi-configtxt.nix

--- pi-configtxt.nix
+++ pi-configtxt.nix
@@ -2,6 +2,7 @@
 
 {
   hardware = {
+    i2c.enable = true;
     raspberry-pi.config.all = {
       dt-overlays = {
         cma = {
@@ -12,6 +13,25 @@
               value = "128000000";
             };
           };
+        };
+        imx219 = { # camera model
+          enable = true;
+          params = {};
+        };
+      };
+ 
+     base-dt-params = {
+        camera_auto_detect = {
+          enable = true;
+          value = false;
+        };
+        i2c_arm = {
+          enable = true;
+          value = "on";
+        };
+        i2c_arm_baudrate = {
+          enable = true;
+          value = 1000000;
         };
       };
     };
