409f193085fa0055e2eec355ed6462691ced3526
Caleb Husovich <147654545+chusovich@users.noreply.github.com>
2026-01-04 00:25:34 -0500

    Update pi-configtxt.nix

--- pi-configtxt.nix
+++ pi-configtxt.nix
@@ -1,16 +1,12 @@
 { config, pkgs, lib, ... }:
 
 {
-  hardware = {
-    i2c.enable = true;
-    raspberry-pi.config.all = {
-      dt-overlays = {
-        vc4-kms-v3d = {
-          enable = true;
-          params = { 
-            cma-128.enable = true; # set cma memory to 128MB, defaults of 512MB is to high for RPi4 4GM RAM 
-          };
-        };
+  hardware.raspberry-pi.config.all = {
+  dt-overlays = {
+    vc4-kms-v3d = {
+      enable = true;
+      params = { 
+        cma-128.enable = true; # set cma memory to 128MB, defaults of 512MB is to high for RPi4 4GM RAM 
       };
     };
   };
